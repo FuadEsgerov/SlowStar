7606f3af9f3e679b920e91cdef8ef95330a94a8d
FuadEsgerov <fuadra@code.edu.az>
2020-09-01 18:12:22 +0400

    Add Some DB context

---
+++ Repository/Data/AppDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<OurTeam> OurTeams { get; set; }
+        public DbSet<Faq> Faqs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Banner> Banners { get; set; }
+        public DbSet<DeliveryInformation> DeliveryInformations { get; set; }
+        public DbSet<LookBook> LookBooks { get; set; }
+        public DbSet<LookBookPhoto> LookBookPhotos { get; set; }
+        public DbSet<PrivacyPolicy> PrivacyPolicies { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<Label> Labels { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductDiscount> ProductDiscounts { get; set; }
+        public DbSet<ProductOption> ProductOptions { get; set; }
+        public DbSet<ProductOptionItem> ProductOptionItems { get; set; }
+        public DbSet<ProductPhoto> ProductPhotos { get; set; }
+        public DbSet<ProductReview> ProductReviews { get; set; }
+        public DbSet<ProductSpec> ProductSpecs { get; set; }
+        public DbSet<Setting> Setting { get; set; }
+        public DbSet<ShopCollection> ShopCollections { get; set; }
+        public DbSet<SliderItem> SliderItems { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
--- SlowStar/Startup.cs
+++ SlowStar/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Repository.Data;
 
 namespace SlowStar
 {
@@ -24,6 +26,12 @@ namespace SlowStar
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<AppDbContext>(options =>
+       options.UseSqlServer(Configuration.GetConnectionString("Default"),
+       x => x.MigrationsAssembly("Repository")));
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
